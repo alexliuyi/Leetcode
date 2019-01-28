eb71ea1f4bbf2b2b2a220f0375455f6eaca58cab
alexliuyi <alexliuyi1987@hotmail.com>
2019-01-28 13:58:19 -0600

    Create 160. Intersection of Two Linked Lists.cpp

---
+++ 160. Intersection of Two Linked Lists.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+ /************** Methon 1 ********************/
+ 
+#include <set>
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        std::set<ListNode*> node_set;
+        while(headA){
+            node_set.insert(headA);
+            headA = headA->next;
+        }
+        while(headB){
+            if(node_set.find(headB)!=node_set.end()){
+                return headB;    
+            }
+                headB = headB->next;
+        }
+        return NULL;
+    }
+};
