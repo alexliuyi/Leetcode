427723b80276819f843727436077ae194169e12a
alexliuyi <alexliuyi1987@hotmail.com>
2019-01-28 14:54:49 -0600

    Create 160. Intersection of Two Linked Lists.py

---
+++ 160. Intersection of Two Linked Lists.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+/***************     Methon 1 ******************/
+
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        node_set = set()
+        while headA!=None:
+            node_set.add(headA)
+            headA = headA.next
+        while headB!=None:
+            if headB in node_set:
+                return headB
+            headB = headB.next
+        return None
+        
+        
+/***************     Methon 2 ******************/
