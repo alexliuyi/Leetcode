64f7a2f328f618ff36596e3587afac3314e4652e
alexliuyi <alexliuyi1987@hotmail.com>
2019-01-29 10:38:39 -0600

    Update 142. Linked List Cycle II.py

--- 142. Linked List Cycle II.py
+++ 142. Linked List Cycle II.py
@@ -22,3 +22,35 @@ class Solution(object):
         
 
 ########################### Method 2 ###################################
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        fast = head
+        slow = head
+        meet = None
+        
+        while fast:
+            slow = slow.next
+            fast = fast.next
+            if fast == None:
+                return None
+            fast = fast.next
+            if fast == slow:
+                meet = fast
+                break
+                
+        while meet and head:
+            if meet == head:
+                return head
+            meet = meet.next
+            head = head.next
+        return None
