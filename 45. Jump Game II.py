6dc5fcf5a4780d12a55611625aeab0af46f506f1
alexliuyi <alexliuyi1987@hotmail.com>
2019-02-04 14:16:27 -0600

    Create 45. Jump Game II.py

---
+++ 45. Jump Game II.py
@@ -0,0 +1,18 @@
+class Solution:
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums)<2:
+            return 0
+        current_max_index = nums[0]
+        pre_max_max_index = nums[0]
+        jump_min = 1
+        for i in range(1,len(nums)):
+            if i > current_max_index:
+                jump_min +=1
+                current_max_index = pre_max_max_index
+            if pre_max_max_index < nums[i]+i:
+                pre_max_max_index = nums[i]+i
+        return jump_min
