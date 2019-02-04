2c094b4a2078f17ecd506e042ad2be1c8f5c557a
alexliuyi <alexliuyi1987@hotmail.com>
2019-02-04 14:23:12 -0600

    Update 473. Matchsticks to Square.cpp

--- 473. Matchsticks to Square.cpp
+++ 473. Matchsticks to Square.cpp
@@ -34,3 +34,53 @@ private:
         return false;
     }
 };
+
+/************************ Method 2 ************************/
+#include <vector>
+
+class Solution {
+public:
+    bool makesquare(vector<int>& nums) {
+        if(nums.size()<4){
+            return false;
+        }    
+        int sum = 0;
+        for (int i=0; i<nums.size();i++){
+            sum += nums[i];
+        }
+        if(sum%4){
+            return false;
+        }
+        int target = sum/4;
+        std::vector<int> ok_sub;
+        std::vector<int> ok_half;
+        
+        int all = 1<<nums.size();    
+        for(int i=0; i < all; i++){
+            int sum=0;
+            for(int j=0; j < nums.size(); j++){
+                if(i & 1<<j){
+                sum += nums[j];
+                }    
+            }
+            if(sum==target){
+                ok_sub.push_back(i);
+            }
+        }
+        for(int i=0; i < ok_sub.size(); i++){
+            for(int j =i+1; j< ok_sub.size(); j++){
+                if((ok_sub[i] & ok_sub[j])==0){
+                    ok_half.push_back(ok_sub[i]|ok_sub[j]);
+                }
+            }
+        }
+        for (int i=0; i < ok_half.size(); i++){
+            for(int j=i+1; j<ok_half.size(); j++){
+                if((ok_half[i] & ok_half[j])==0){
+                    return true;
+                }
+            }
+        }
+        return false;    
+    }   
+};
